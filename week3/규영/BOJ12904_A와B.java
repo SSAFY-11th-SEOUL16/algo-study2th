6314ef5c47d5d1c4f769736dae1f0bcd051ca2a6
gyuoo <yeomky98@naver.com>
2024-02-24 14:55:41 +0900

    염규영 : BOJ 12904 A와 B

---
+++ week3/규영/BOJ12904_A와B.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+// 96ms
+
+public class BOJ12904_A와B {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine(), T = br.readLine();
+        while (S.length() != T.length()) {
+            if (T.charAt(T.length()-1) == 'A') T = T.substring(0, T.length()-1);
+            else T = new StringBuilder(T.substring(0, T.length()-1)).reverse().toString();
+        }
+        System.out.println(S.equals(T) ? 1 : 0);
+    }
+}
