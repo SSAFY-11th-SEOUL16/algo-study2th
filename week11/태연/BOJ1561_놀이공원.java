31aee0dae94d8f17633f498a2a3409bdf2252a88
방태연 <imbhang3@naver.com>
2024-04-12 09:31:21 +0900

    방태연 : BOJ 1561 놀이공원 수정ver
    - 100ms
    - N번째 친구가 타는 시간을 기준으로 이분탐색, 이후 해당 분에 몇번째 기구가 비어있는지 연산

--- week11/태연/BOJ1561_놀이공원.java
+++ week11/태연/BOJ1561_놀이공원.java
@@ -4,65 +4,70 @@ import java.util.*;
 public class Main {
 
 	/*
-	 * - 112ms
+	 * - 100ms
 	 * 
 	 * - N번째 친구가 타는 시간을 기준으로 이분탐색, 이후 해당 분에 몇번째 기구가 비어있는지 연산
 	 */
 
-	static int n,m;
-	static int[] arr;
-	
-	static boolean validate(long x) {
-		long sum = m;
-		
-		for(int i=0; i<m; i++) {
-			sum += x/arr[i];
-			if(sum>=n) return true;
-		}
-		
-		return false;
-	}
-	
-	static int getAns(long x, int n) {
-		if(x==0) return n;
-		
-		long sum = n;
-		for(int i=0; i<m; i++) {
-			sum -= ((x-1)/arr[i])+1;
-		}
-		
-		for(int i=0; i<m; i++) {
-			if(x%arr[i]==0) sum--;
-			if(sum==0) return i+1;
-		}
-		
-		return m;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		arr = new int[m];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<m; i++) arr[i] = Integer.parseInt(st.nextToken());
-		
-		long left = 0; long right = 60000000000L;
-		
-		while(left<right) {
-			long mid = (left+right)/2;
-			if(validate(mid)) {
-				right = mid;
-			}
-			else {
-				left = mid+1;
-			}
-		}
-		
-		System.out.println(getAns(left, n));
-	}
+	 static int n,m;
+	 static int[] arr;
+	 static int[] byTime;
+	 
+	 static boolean validate(long x) {
+		 long sum = m;
+		 
+		 for(int i=1; i<=30; i++) {
+			 sum += (x/i)*byTime[i];
+			 if(sum>=n) return true;
+		 }
+		 
+		 return false;
+	 }
+	 
+	 static int getAns(long x, int n) {
+		 if(x==0) return n;
+		 
+		 long sum = n;
+		 for(int i=0; i<m; i++) {
+			 sum -= ((x-1)/arr[i])+1;
+		 }
+		 
+		 for(int i=0; i<m; i++) {
+			 if(x%arr[i]==0) sum--;
+			 if(sum==0) return i+1;
+		 }
+		 
+		 return 0;
+	 }
+	 
+	 public static void main(String[] args) throws Exception {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 StringTokenizer st = new StringTokenizer(br.readLine());
+		 
+		 n = Integer.parseInt(st.nextToken());
+		 m = Integer.parseInt(st.nextToken());
+		 
+		 arr = new int[m];
+		 byTime = new int[31];
+		 st = new StringTokenizer(br.readLine());
+		 for(int i=0; i<m; i++) {
+			 arr[i] = Integer.parseInt(st.nextToken());
+			 byTime[arr[i]]++;
+		 }
+		 
+		 long left = 0; long right = 60000000000L;
+		 
+		 while(left<right) {
+			 long mid = (left+right)/2;
+			 if(validate(mid)) {
+				 right = mid;
+			 }
+			 else {
+				 left = mid+1;
+			 }
+		 }
+		 
+		 System.out.println(getAns(left, n));
+	 }
 
 }
